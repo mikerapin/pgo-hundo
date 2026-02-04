a6ce52b5807f2c043319efe99f8ebc16ddb6cfd6
mike ¯\_(ツ)_/¯ <1847895+mikerapin@users.noreply.github.com>
2026-02-04 00:08:35 -0500

    change deploy?

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import preact from '@preact/preset-vite';
 import { defineConfig } from 'vite';
 
-export default defineConfig(({ command }) => {
+export default defineConfig(() => {
   return {
     plugins: [
       preact({
