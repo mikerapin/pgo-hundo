3b5c58887d0e475d2270a32240372725490ef140
mike ¯\_(ツ)_/¯ <1847895+mikerapin@users.noreply.github.com>
2026-02-03 23:26:50 -0500

    test

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ export default defineConfig(({ command }) => {
   if (command === 'build') {
     baseUrl = '/pgo-hundo/';
   }
+  console.log('setting base url to: ', baseUrl)
   return {
     base: baseUrl,
     plugins: [
