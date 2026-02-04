2ba14f292e9c7de207b36416c97f598b540e8551
mike ¯\_(ツ)_/¯ <1847895+mikerapin@users.noreply.github.com>
2026-02-04 00:06:22 -0500

    remove base-url changes

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,7 @@ import preact from '@preact/preset-vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig(({ command }) => {
-  let baseUrl = '';
-  if (command === 'build') {
-    baseUrl = '/pgo-hundo/';
-  }
-  console.log('setting base url to: ', baseUrl)
   return {
-    base: baseUrl,
     plugins: [
       preact({
         prerender: {
