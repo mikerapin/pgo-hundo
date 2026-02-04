823461a720a6c6713ecb9a467811a7ede4625b27
mike ¯\_(ツ)_/¯ <1847895+mikerapin@users.noreply.github.com>
2026-02-04 09:33:00 -0500

    convert dropdown to select (for now...)

--- src/components/SelectEvent/index.tsx
+++ src/components/SelectEvent/index.tsx
@@ -7,24 +7,39 @@ export const SelectEvent = () => {
   const { currentEvent, setCurrentEvent, events } = useContext(CurrentEventContext);
 
   const isSelectedEvent = (event: EventData) => event.name === currentEvent?.name && event.startDate === currentEvent?.startDate;
-
   return (
-    <details className="dropdown">
-      <summary className="secondary">Select event</summary>
-      <ul dir="rtl">
-        {events.map(event =>
-          <li>
-            <a
-              className={isSelectedEvent(event) ? 'secondary' : ''}
-              href="#"
-              onClick={() => setCurrentEvent(event)}
-              style={{ textDecoration: isSelectedEvent(event) ? 'underline' : 'none' }}
-            >
-              {event.name} | <small>{new Date(event.startDate).toLocaleDateString()}</small>
-            </a>
-          </li>
-        )}
-      </ul>
-    </details>
+    <select name="selected-event" aria-label="Select an event..."
+            onChange={(e) => setCurrentEvent(events.find(event => (event.name + event.startDate) === e.currentTarget.value))}>
+      <option selected disabled value="">
+        Select an event...
+      </option>
+      {events.map(event =>
+        <option value={event.name + event.startDate} selected={isSelectedEvent(event)}>
+          {event.name} | <small>{new Date(event.startDate).toLocaleDateString()}</small>
+        </option>
+      )}
+    </select>
   );
+
+  // const isSelectedEvent = (event: EventData) => event.name === currentEvent?.name && event.startDate === currentEvent?.startDate;
+  //
+  // return (
+  //   <details className="dropdown">
+  //     <summary className="secondary">Select event</summary>
+  //     <ul dir="rtl">
+  //       {events.map(event =>
+  //         <li>
+  //           <a
+  //             className={isSelectedEvent(event) ? 'secondary' : ''}
+  //             href="#"
+  //             onClick={() => setCurrentEvent(event)}
+  //             style={{ textDecoration: isSelectedEvent(event) ? 'underline' : 'none' }}
+  //           >
+  //             {event.name} | <small>{new Date(event.startDate).toLocaleDateString()}</small>
+  //           </a>
+  //         </li>
+  //       )}
+  //     </ul>
+  //   </details>
+  // );
 };
