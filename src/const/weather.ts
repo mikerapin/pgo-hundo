247e9701172f6e43013cd0301ca640c1b98ce350
mike ¯\_(ツ)_/¯ <1847895+mikerapin@users.noreply.github.com>
2026-03-02 01:08:28 -0500

    update weather, add new events for march

--- src/const/weather.ts
+++ src/const/weather.ts
@@ -1,9 +1,10 @@
 export enum Weather {
-  CLEAR = 'clear',
-  CLOUDY = 'cloudy',
-  FOG = 'fog',
-  RAIN = 'rain',
-  SNOW = 'snow',
-  SUNNY = 'sunny',
-  WINDY = 'windy',
+CLEAR = 'clear',
+CLOUDY = 'cloudy',
+FOG = 'fog',
+PARTLY_CLOUDY = 'partly cloudy',
+RAIN = 'rain',
+SNOW = 'snow',
+SUNNY = 'sunny',
+WINDY = 'windy',
 }
